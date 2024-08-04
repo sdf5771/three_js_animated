dd641e585cd524e804465fe3da8f8c2e4e7d65b6
seobisback <seobisback@gmail.com>
2024-08-04 13:35:13 +0900

    install. three js, three-gltf-loader,  create. AnimateApp class

--- app.js
+++ app.js
@@ -1,5 +1,47 @@
+'use strict'
+import * as THREE from 'three';
+import GLTFLoader from 'three-gltf-loader';
+
+class AnimateApp{
+    constructor(){
+        const mainElement = document.querySelector('#mainApp');
+        this._mainElement = mainElement;
+
+        const renderer = new THREE.webGLRenderer({
+            antialias: true
+        });
+
+        this._renderer = renderer;
+
+        const scene = new THREE.Scene();
+        this._scene = scene;
+
+        this._setupCamera();
+        this._setupLight();
+        this._setupModel();
+        this._setupControls();
+
+        window.onresize = this.resize.bind(this);
+        this.resize();
+
+        requestAnimationFrame(this.render.bind(this));
+    }
+
+    _setupCamera(){
+
+    }
+    _setupLight(){
+
+    }
+    _setupModel(){
+
+    }
+    _setupControls(){
+        
+    }
+}
 function main(){
-    console.log('js is running')
+    
 }
 
 main();
